70a6e061873513f25fffed215e00055d88ce31bb
Alethea Butler <alethea@alethea.io>
2014-03-30 13:58:34 -0400

    Cleaned up gendata

--- simpe_epi.py
+++ simpe_epi.py
@@ -10,68 +10,69 @@ University of Michigan
 October 8, 2009
 '''
 
-import random 
+import random
 import pylab as pl
 
-'''
-Generates randomized data based on a Simple SIR Model.
 
-Inputs: Takes the susceptible, infected, and recovered populations
-        Takes the infection rate or likeliness to go from S to I
-        Takes the recovery rate or likeliness to go from I to R
-
-Returns: Array of the number of infected at each time step 
-'''
 def gendataSIR(s, i, r, gamma, beta):
-    #number of susceptibles
+    '''
+    Generates randomized data based on a Simple SIR Model.
+
+    Inputs: Takes the susceptible, infected, and recovered populations
+            Takes the infection rate or likeliness to go from S to I
+            Takes the recovery rate or likeliness to go from I to R
+
+    Returns: Array of the number of infected at each time step
+    '''
+    # number of susceptibles
     S = s
-    #seeding the outbreak with one infectious individual
+    # seeding the outbreak with one infectious individual
     I = i
-    #number of recovered
-    R = r 
-     
-    #total poopulation in system 
+    # number of recovered
+    R = r
+
+    # total poopulation in system
     N = S+I+R
-    
+
     N = float(N)
-    #time
+    # time
     t = 0
-      
+
     sList = []
     iList = []
-    rList = [] 
-    #number of newly infected people on each step
-    newIList = [] 
-     
+    rList = []
+    # number of newly infected people on each step
+    newIList = []
+
     while I > 0:
         newI = 0
-        
-        #there is a single random trial for each susceptible individual
-        for i in range(S):
-            #frequency dependent
+
+        # there is a single random trial for each susceptible individual
+        for _ in xrange(S):
+            # frequency dependent
             if random.random() < beta*(I/N):
                 newI += 1
-            
-            #Density dependent
+
+            # Density dependent
             # if random.random() < b*I:
             #    newI += 1
-        
+
         recoverI = 0
-        for i in range(I):
+        for _ in xrange(I):
             if random.random() < gamma:
                 recoverI += 1
-        
-        #Update values        
+
+        # Update values
         S -= newI
         I += (newI - recoverI)
         R += recoverI
-        
-        #add values at this timestep
+
+        # add values at this timestep
         sList.append(S)
         iList.append(I)
         rList.append(R)
         newIList.append(newI)
-        
+
         t += 1
 
     return iList
