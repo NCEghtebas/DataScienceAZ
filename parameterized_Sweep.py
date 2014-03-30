93e9dc61b020fa39703b08bdc9d67b9b7b5244f2
NCEghtebas <nceghtebas@hotmail.com>
2014-03-30 16:21:39 -0400

    Cleaned up code

--- parameterized_Sweep.py
+++ parameterized_Sweep.py
@@ -25,29 +25,9 @@ def sweep_range(start, stop, count):
 		yield curr
 		curr += incr
 
-#b = .09
-#g = .05
 
-S = 1000 
-#seeding the outbreak with one infectious individual
-I = 10
-#number of recovered
-R = 0 
-
-#likelyness to get infected is high
-hbeta= [0.09, 0.7, 0.6, 0.5]  
-#outbreak never happens at 0.9
-#likeliness to get infected is low
-lbeta= [0.01, 0.05, 0.1, 0.2]
-
-#likelyness to get cured (or die) is high
-hgamma= [0.9, 0.8, 0.7, 0.6]
-#likelyness to get cured (or die) is low
-lgamma= [0.01,0.05, 0.1, 0.2]
 
 subplots, (sp, ip, rp) = pl.subplots(3, sharex=True, sharey=True)
-
-
 for sw in sweep(np.logspace(np.log10(0.9), np.log10(0.01), 25), np.logspace(np.log10(0.9), np.log10(0.1), 25)):
 	s, i, r = zip(*sw)
 	sp.plot(s)
@@ -56,22 +36,4 @@ for sw in sweep(np.logspace(np.log10(0.9), np.log10(0.01), 25), np.logspace(np.l
 prepare_figure()
 subplots.subplots_adjust(hspace=0)
 pl.show()
-# print "done hbeta"
-# sweep(lbeta)
-# print "done lbeta"
-# sweep(hgamma
-# print "done hgamma"
-# sweep(lgamma)
-# print "done lgamma"
 
-'''
-xx=[]
-x = []
-for i in range(5):
-    while len(x) < 200:
-        x= gendata(S, I, R, 0.09, 0.05)
-        xx.append(x)
-    plotdata(x)
-    #print i, x
-    x = []
-'''
