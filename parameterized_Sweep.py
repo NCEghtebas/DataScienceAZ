f812da84634e4f56565f723b861875fbc671f356
Alethea Butler <alethea@alethea.io>
2014-03-30 16:33:03 -0400

    Cleaned up code in sweep, for importing

--- parameterized_Sweep.py
+++ parameterized_Sweep.py
@@ -5,35 +5,35 @@ import numpy as np
 
 
 def prepare_figure(title="Simple SIR Model Number of Infected"):
-	pl.suptitle(title)
-	pl.xlabel("Time Steps")
-	pl.ylabel("Number of Infected")
+    pl.suptitle(title)
+    pl.xlabel("Time Steps")
+    pl.ylabel("Number of Infected")
 
 
-def sweep(gs, bs):
-	for g in gs:
-		for b in bs:
-			res = gendataSIR(S,I,R, g, b)
-			print 'g:', g, 'b:', b, 'len:', len(res)
-			yield res
+def sweep(s, i, r, gs, bs):
+    for g in gs:
+        for b in bs:
+            res = gendataSIR(s, i, r, g, b)
+            yield (g, b, res)
 
 
 def sweep_range(start, stop, count):
-	curr = start
-	incr = (stop - start) / count
-	for _ in xrange(count):
-		yield curr
-		curr += incr
+    return np.logspace(np.log10(start), np.log10(stop), count)
 
 
+def main():
+    subplots, (sp, ip, rp) = pl.subplots(3, sharex=True, sharey=True)
+    sr = sweep_range(0.99, 0.01, 25)
+    for g, b, sw in sweep(1000, 5, 0, sr, sr):
+        print 'g:', g, 'b:', b, 'len:', len(sw)
+        s, i, r = zip(*sw)
+        sp.plot(s)
+        ip.plot(i)
+        rp.plot(r)
+    prepare_figure()
+    subplots.subplots_adjust(hspace=0)
+    pl.show()
 
-subplots, (sp, ip, rp) = pl.subplots(3, sharex=True, sharey=True)
-for sw in sweep(np.logspace(np.log10(0.9), np.log10(0.01), 25), np.logspace(np.log10(0.9), np.log10(0.1), 25)):
-	s, i, r = zip(*sw)
-	sp.plot(s)
-	ip.plot(i)
-	rp.plot(r)
-prepare_figure()
-subplots.subplots_adjust(hspace=0)
-pl.show()
 
+if __name__ == '__main__':
+    main()
