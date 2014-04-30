298c19a1289a7919fc1f84494bd41adebf0d41d4
NCEghtebas <nceghtebas@hotmail.com>
2014-04-30 19:22:42 -0400

    creates a table for each statistic from world factbook we parsed.

--- retrieve.py
+++ retrieve.py
@@ -1,6 +1,16 @@
 import os
 import sys
 import re
+from database import select, connect
+from collections import namedtuple
+
+
+def getcountrylist():
+	countries= select("earsnet", ["country"], distinct = True)
+	countrylist=[]
+	for country in countries:
+		countrylist.append(country[0].decode('utf8'))
+	return countrylist
 
 def finddata(country, fn):
 	with open(fn, 'r') as f:
@@ -24,13 +34,11 @@ def raw(data):
 	return data
 
 def currency(data):
+	conversion = {'million': 1e6, 'billion': 1e9, 'trillion': 1e12}
 	m = re.search(r'\$([0-9\.]+)\s+(\w*)\s?',data)
 	if m is None:
 		return None
-	if(m.group(2) == 'billion'):
-		return float(m.group(1) ) * 1e9
-	elif(m.group(2) == 'trillion'):
-		return float(m.group(1)) *1e12
+	return float(m.group(1)) * conversion[m.group(2)]
 
 
 def percentage(data):
@@ -57,20 +65,59 @@ def percentageGroup(*keywords):
 		return res
 	return innerPercentageGroup
 
+Stat = namedtuple('Stat', ['func', 'name', 'columns', 'count'])
+
 stats= {
-	2001: currency,2002: percentage,2003: percentage,2012:percentage,
-	2021: raw,2032:raw,2034: percentage, 2046:percentage,
-	2048:percentageGroup('agriculture', 'services', 'industry'),
-	2056: currency,2059: raw, 2066:rate}
+	2001: Stat(currency, 'gdp', 'gdp MONEY', 1),
+	2002: Stat(percentage, 'pop_growth_rate', 'rate REAL', 1),
+	2003: Stat(percentage, 'real_gdp', 'gdp MONEY', 1),
+	2021: Stat(raw, 'natural_hazards', 'hazards TEXT', 1),
+	2032: Stat(raw, 'current_environmental_issues', 'enviornmental TEXT', 1),
+	2034: Stat(percentage, 'militray_expenditure', 'military REAL', 1), 
+	2046: Stat(percentage, 'poverty_population', 'poverty REAL', 1),
+ 	2048: Stat(percentageGroup('agriculture', 'services', 'industry'),
+ 			   'labor_force',
+ 			   'agriculture REAL, services REAL, industry REAL',
+ 			   3),
+	2056: Stat(currency, 'budget' ,'budget MONEY', 1),
+	2059: Stat(raw, 'climate_description', 'climate TEXT', 1), 
+	2066: Stat(rate, 'death_rate', 'rate REAL', 1)}
+
+def create_tables(conn):
+	cur = conn.cursor()
+	for stat in stats.values():
+		cur.execute('DROP TABLE IF EXISTS {0};'.format(stat.name))
+		cur.execute(
+			'CREATE TABLE {0} (year SMALLINT, country TEXT, {1});'.format(
+				stat.name, stat.columns))
+
+def insert_row(conn, year, country, stat, value):
+	cur = conn.cursor()
+	placeholder = '%s, %s' + ', %s' * stat.count
+	values = [year, country]
+	if isinstance(value, str):
+		values.append(value)
+	else:
+		try:
+			values.extend(value)
+		except TypeError:
+			values.append(value)
+	cur.execute(
+		'INSERT INTO {0} VALUES ({1});'.format(stat.name, placeholder),
+		values)
 
 def main():
-	for statnum, function in stats.items():
-		print statnum
-		for datum in retreivedata('France', statnum):
-			if function is not None and datum[1] is not None:
-				print (datum[0], function(datum[1]))
-				pass
-			# print datum
+	conn = connect(sys.argv[1])
+	create_tables(conn)
+	for country in getcountrylist():
+		for statnum, stat in stats.items():
+			for datum in retreivedata(country, statnum):
+				if datum[1] is not None:
+					value = stat.func(datum[1])
+					if value is not None:
+						print datum[0], value
+						insert_row(conn, datum[0], country, stat, value)
+	conn.commit()
 
 if __name__ == '__main__':
     main()
